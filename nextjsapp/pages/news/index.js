e5d526aef89a398f23d66f8470b7a62fc980423a
zsofi <zsofiath@gmail.com>
2022-03-19 18:52:26 +0100

    navigation

--- nextjsapp/pages/news/index.js
+++ nextjsapp/pages/news/index.js
@@ -1,5 +1,17 @@
+import Link from 'next/link'
 function NewsPage () {
-    return <div>The News</div>
+    return <div>The News
+
+        <div>
+            {/*  ez is működik, de oldal újratöltést csinál */}
+            <a href="/news/blabla">Link</a>
+            <a href="/news/blabla2">Link2</a>
+
+            {/* Nem tölt újra */}
+            <Link href="/news/blabla">Link</Link>
+            <Link href="/news/blabla2">Link2</Link>
+        </div>
+    </div>
 }
 
 export default NewsPage
