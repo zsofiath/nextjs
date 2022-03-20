309fc5f8b457000afa987e491cbe1dfeb3eec752
zsofi <zsofiath@gmail.com>
2022-03-20 13:56:58 +0100

    getStaticProps

--- nextjsapp/pages/index.js
+++ nextjsapp/pages/index.js
@@ -1,12 +1,41 @@
 import MeetupList from '../components/meetups/MeetupList'
-
-function HomePage () {
-    return <MeetupList meetups={[
-        {id: '1',
-            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/DandelionFlower.jpg/330px-DandelionFlower.jpg',
-            title: 'Jaskier',
-            address: 'Sintra'}
-    ]}></MeetupList>
+const dummy = [
+    {id: '1',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/DandelionFlower.jpg/330px-DandelionFlower.jpg',
+        title: 'Jaskier',
+        address: 'Sintra'}
+]
+function HomePage (props) {
+    return <MeetupList meetups={props.meetups}></MeetupList>
 }
+/**
+ * Csak a pages folderben használható
+ * Ez kell legyen a neve
+ * Megvárja a datát
+ */
+export async function getStaticProps(){
+    // ide jöhet a BE kód
+    // ez nem megy le a klienshez
+    /**Ét tényleg a BE-n fut, ahol x időként (revalidate) lefut, és generálja a leküldendő html-t*/
+    const p =  await new Promise((res, rej) => {
+        setTimeout(() => {
+            res()
+        }, 500);
+    })
+let i = 2
+        setTimeout(() => {
+        dummy.push({id: ''+i,
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/DandelionFlower.jpg/330px-DandelionFlower.jpg',
+        title: 'Jaskier',
+        address: 'Sintra'})
+        i++;
+    }, 10000);
 
+    return {
+        props: {
+            meetups: dummy
+        },
+        revalidate: 10 //mindig generálja az oldalt - incremental static generation
+    }
+}
 export default HomePage
