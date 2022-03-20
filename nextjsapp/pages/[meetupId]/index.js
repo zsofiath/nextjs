6147e794c1cff417e08a3097200704a151d38c53
zsofi <zsofiath@gmail.com>
2022-03-20 18:28:55 +0100

    getStaticPaths

--- nextjsapp/pages/[meetupId]/index.js
+++ nextjsapp/pages/[meetupId]/index.js
@@ -1,11 +1,43 @@
 
 function MeetupDetails () {
     return <>
-    <image src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/DandelionFlower.jpg/330px-DandelionFlower.jpg' alt=''/>
+    {/* <image src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/DandelionFlower.jpg/330px-DandelionFlower.jpg' alt=''/> */}
     <h1>Meet</h1>
     <address>Street</address>
     <p>Descr</p>
     </>
+}
+
+/**Dynamic page && getSteticProps  -> getStaticPats is required*/
+
+export async function getStaticPaths() {
+    return {
+        fallback: false, // ha nem talált routre megy, akkor 404 vagy generálja le gyorsan
+        paths: [
+            {params: {
+                meetupId: 'm1'
+            }},
+            {params: {
+                meetupId: 'm2'
+            }}
+        ]
+    }
+}
+
+export async function getStaticProps(context){
+    // ide jöhet a BE kód
+    // ez nem megy le a klienshez
+    // Ét tényleg a BE-n fut, ahol x időként (revalidate) lefut, és generálja a leküldendő html-t
+
+const meetupId = context.params.meetupId;
+console.log(meetupId);
+
+    return {
+        props: {
+            meetups: {}
+        },
+        revalidate: 10 //mindig generálja az oldalt - incremental static generation
+    }
 }
 
 export default MeetupDetails
--- nextjsapp/pages/index.js
+++ nextjsapp/pages/index.js
@@ -13,23 +13,15 @@ function HomePage (props) {
  * Ez kell legyen a neve
  * Megvárja a datát
  */
-/*export async function getStaticProps(){
+
+
+
+
+export async function getStaticProps(){
     // ide jöhet a BE kód
     // ez nem megy le a klienshez
     // Ét tényleg a BE-n fut, ahol x időként (revalidate) lefut, és generálja a leküldendő html-t
-    const p =  await new Promise((res, rej) => {
-        setTimeout(() => {
-            res()
-        }, 500);
-    })
-let i = 2
-        setTimeout(() => {
-        dummy.push({id: ''+i,
-        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/DandelionFlower.jpg/330px-DandelionFlower.jpg',
-        title: 'Jaskier',
-        address: 'Sintra'})
-        i++;
-    }, 10000);
+
 
     return {
         props: {
@@ -37,13 +29,13 @@ let i = 2
         },
         revalidate: 10 //mindig generálja az oldalt - incremental static generation
     }
-}*/
+}
 
 /**
  * On server after deployment
  * for every incoming request it will run
  */
-export async function getServerSideProps(context){
+/*export async function getServerSideProps(context){
     const req = context.req;
     const res = context.res;
 
@@ -59,5 +51,5 @@ export async function getServerSideProps(context){
         },
 
     }
-}
+}*/
 export default HomePage
