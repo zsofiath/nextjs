68d75ca0967df2099b5d9b565e912c4235acc083
zsofi <zsofiath@gmail.com>
2022-03-20 11:52:39 +0100

    view meetup

--- nextjsapp/pages/[meetupId]/index.js
+++ nextjsapp/pages/[meetupId]/index.js
@@ -1,6 +1,11 @@
 
-function MeetupPage () {
-
+function MeetupDetails () {
+    return <>
+    <image src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/DandelionFlower.jpg/330px-DandelionFlower.jpg' alt=''/>
+    <h1>Meet</h1>
+    <address>Street</address>
+    <p>Descr</p>
+    </>
 }
 
-export default MeetupPage
+export default MeetupDetails
