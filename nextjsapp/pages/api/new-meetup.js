a3853a1cb8e24f1136a4ccc2c88a38cab1a5011f
zsofi <zsofiath@gmail.com>
2022-08-13 15:34:57 +0200

    connect to mongodb and insert data

--- nextjsapp/pages/api/new-meetup.js
+++ nextjsapp/pages/api/new-meetup.js
@@ -1,8 +1,21 @@
+import {MongoClient} from 'mongodb'
+
+
+
 // post: api/new-meetup
 
-function handler (res, req) {
+ async function handler (res, req) {
     if(req.method == 'POST') {
         const data = req.body
+
+        const client = await MongoClient.connect('mongodb+srv://user1:12345@cluster0.3y9jmye.mongodb.net/meetups?retryWrites=true&w=majority')
+        const db = client.db()
+        const meetupColections = db.collection('meetups')
+        const result = await meetupColections.insertOne({data})
+        console.log(result);
+
+        client.close()
+        res.status(201).json({message: 'Meetup inserted!'})
     }
 }
 
